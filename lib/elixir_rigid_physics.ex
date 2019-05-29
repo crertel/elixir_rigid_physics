0a29b6c348c6d196d3207b05c7334d803e17b3dd
Chris Ertel <chris@kedagital.com>
2019-05-29 13:45:48 -0400

    Start stubbing in Elixir project.

---
+++ lib/elixir_rigid_physics.ex
@@ -0,0 +1,18 @@
+defmodule ElixirRigidPhysics do
+  @moduledoc """
+  Documentation for ElixirRigidPhysics.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirRigidPhysics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_rigid_physics_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirRigidPhysicsTest do
+  use ExUnit.Case
+  doctest ElixirRigidPhysics
+
+  test "greets the world" do
+    assert ElixirRigidPhysics.hello() == :world
+  end
+end
