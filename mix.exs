f1138f88806c4dfaaa78e0019d67b60c57d827ec
Chris Ertel <chris@kedagital.com>
2019-05-29 13:56:38 -0400

    Add missing deps for code quality.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule ElixirRigidPhysics.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +28,11 @@ defmodule ElixirRigidPhysics.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.0.3", only: :dev},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.19.3", only: :dev},
+      {:excoveralls, "~> 0.11.1", only: :test},
+      {:inch_ex, "~> 2.0.0", only: :docs}
     ]
   end
 end
