d0ef9b0f4ae239abcef910c0da170304a9e02b2f
Chris Ertel <chris@kedagital.com>
2019-06-11 22:17:48 -0400

    Start adding information for dynamics.

--- lib/bodies/box.ex
+++ lib/bodies/box.ex
@@ -1,7 +1,7 @@
 defmodule ElixirRigidPhysics.Bodies.Box do
   alias Graphmath.Mat44
 
-  def make(l, w, d, {tx, ty, tz} = _pos)
+  def make(l, w, d, {tx, ty, tz} = _pos, {_rx,_ry,_rz,_rw} = rotquat, {_mx, _my, _mz} = moi, mass)
       when is_number(l) and l > 0 and is_number(w) and w > 0 and is_number(d) and d > 0 do
     xform = Mat44.make_translate(tx, ty, tz)
 
@@ -10,11 +10,16 @@ defmodule ElixirRigidPhysics.Bodies.Box do
       length: l,
       width: w,
       depth: d,
+
+      rotation: rotquat,
+      mass: mass,
+      moi: moi,
+
       transform: xform
     }
   end
 
   def make(l, w, d)
       when is_number(l) and l > 0 and is_number(w) and w > 0 and is_number(d) and d > 0,
-      do: make(l, w, d, {0.0, 0.0, 0.0})
+      do: make(l, w, d, {0.0, 0.0, 0.0}, {0.0,0.0,0.0,0.0}, {0.0,0.0,0.0}, 1)
 end
