f88e762ea90c142e6527d41a82867249660aec81
Chris Ertel <chris@kedagital.com>
2019-06-20 15:58:10 -0400

    Integrate angular velocity now!

--- lib/dynamics/dynamics.ex
+++ lib/dynamics/dynamics.ex
@@ -20,30 +20,11 @@ defmodule ElixirRigidPhysics.Dynamics do
       new_position = Vec3.scale(linear_velocity, dt)|> Vec3.add(position)
 
       # integrate angular velocity
-
-      half_dt = dt / 2.0
-      ang_vel_quat = {0, avx * half_dt, avy * half_dt, avz * half_dt}
-      new_orientation_temp = Quatern.multiply(ang_vel_quat, orientation)
-      IO.inspect({dt, new_orientation_temp, ang_vel_quat}, label: "ROTROTROT")
-      new_orientation = if Quatern.norm(new_orientation_temp) == 0 do
-        new_orientation_temp
-      else
-        new_orientation_temp |> Quatern.normalize()
-      end
-
-      #half_dt = dt / 2.0
-      #new_orientation = {
-      #  -half_dt *((avx * ox) + (avy * oy) + (avz * oz)),
-      #  half_dt * ((avx * ow) + (avy * oz) - (avz * oy)),
-      #  half_dt * ((avy * ow) + (avz * ox) - (avx * oz)),
-      #  half_dt * ((avz * ow) + (avx * oy) - (avy * ox))
-      #}
-
-      IO.inspect(new_orientation, label: "ROTROTROT")
+      new_orientation = Quatern.integrate(orientation, angular_velocity, dt)
 
       new_body = Map.merge(b, %{
         position: new_position,
-        orientation: new_orientation #|> Quatern.normalize()
+        orientation: new_orientation
       })
 
       {r, new_body}
