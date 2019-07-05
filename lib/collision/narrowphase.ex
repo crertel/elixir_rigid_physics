3486cb12808c8e779b940d23f0bef664b963dc56
Chris Ertel <chris@kedagital.com>
2019-07-05 17:07:10 -0400

    Restore test_intersection/2 to narrowphase.

--- lib/collision/narrowphase.ex
+++ lib/collision/narrowphase.ex
@@ -4,14 +4,33 @@ defmodule ElixirRigidPhysics.Collision.Narrowphase do
 
   Supported:
   * sphere-sphere
+  * sphere-capsule
 
   Planned:
-  * sphere-capsule
+  * capsule-capsule
   * box-sphere
   * capsule-box
 
   Check [here](http://media.steampowered.com/apps/valve/2015/DirkGregorius_Contacts.pdf) for a good summary of things.
   """
+
+  require ElixirRigidPhysics.Dynamics.Body, as: Body
+  require ElixirRigidPhysics.Geometry.Sphere, as: Sphere
+  require ElixirRigidPhysics.Geometry.Capsule, as: Capsule
+  require ElixirRigidPhysics.Collision.ContactManifold, as: ContactManifold
+
+  alias ElixirRigidPhysics.Collision.Intersection.SphereSphere
+  alias ElixirRigidPhysics.Collision.Intersection.SphereCapsule
+
+  @doc """
+  Tests the intersection of two shapes.
+  """
+  @spec test_intersection( Body.body, Body.body) :: ContactManifold.contact_manifold()
+  def test_intersection( Body.body(shape: Sphere.sphere()) = a, Body.body(shape: Sphere.sphere()) = b),
+    do: SphereSphere.check(a, b)
+
+  def test_intersection( Body.body(shape: Sphere.sphere()) = a, Body.body(shape: Capsule.capsule()) = b),
+    do: SphereCapsule.check(a, b)
 end
 
 # iex> # Check non-touching capsules
