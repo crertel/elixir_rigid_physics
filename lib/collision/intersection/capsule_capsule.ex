09c7183470a5d392646150615489d0a99c0a4330
Chris Ertel <chris@kedagital.com>
2019-07-06 23:03:47 -0400

    Rough in the logic for capsule-capsule, bit more to do.

--- lib/collision/intersection/capsule_capsule.ex
+++ lib/collision/intersection/capsule_capsule.ex
@@ -24,7 +24,7 @@ defmodule ElixirRigidPhysics.Collision.Intersection.CapsuleCapsule do
     iex> require ElixirRigidPhysics.Dynamics.Body, as: Body
     iex> a = Body.body( shape: Capsule.capsule(axial_length: 1, cap_radius: 0.5), position: {0.0, 0.0, 0.0})
     iex> b = Body.body( shape: Capsule.capsule(axial_length: 1, cap_radius: 0.5), position: {5.0, 0.0, 0.0})
-    iex> Intersection.check(a,b)
+    iex> CapsuleCapsule.check(a,b)
     :no_intersection
 
     iex> # Check coincident capsules
@@ -33,7 +33,7 @@ defmodule ElixirRigidPhysics.Collision.Intersection.CapsuleCapsule do
     iex> require ElixirRigidPhysics.Dynamics.Body, as: Body
     iex> a = Body.body( shape: Capsule.capsule(axial_length: 1, cap_radius: 0.5), position: {0.0, 0.0, 0.0})
     iex> b = Body.body( shape: Capsule.capsule(axial_length: 1, cap_radius: 0.5), position: {0.0, 0.0, 0.0})
-    iex> Intersection.check(a,b)
+    iex> CapsuleCapsule.check(a,b)
     :coincident
 
     iex> # Check grazing cap contact
@@ -42,14 +42,45 @@ defmodule ElixirRigidPhysics.Collision.Intersection.CapsuleCapsule do
     iex> require ElixirRigidPhysics.Dynamics.Body, as: Body
     iex> a = Body.body( shape: Capsule.capsule(axial_length: 3.0, cap_radius: 0.5), position: {0.0, 0.0, 0.0})
     iex> b = Body.body( shape: Capsule.capsule(axial_length: 2.0, cap_radius: 1.0), position: {0.0, 4.0, 0.0})
-    iex> Intersection.check(a,b)
+    iex> CapsuleCapsule.check(a,b)
     {:contact_manifold, {{:contact_point, {0.0, 2.0, 0.0}, 1.0}}, {0.0, 1.0, 0.0}}
 
   """
   def check(
-        Body.body(shape: Capsule.capsule(cap_radius: cr_a), position: p_a, orientation: o_b),
-        Body.body(shape: Capsule.capsule(cap_radius: cr_b) = c, position: p_b, orientation: o_b)
+        Body.body(shape: Capsule.capsule(cap_radius: cr_a) = cap_a, position: p_a, orientation: o_b),
+        Body.body(shape: Capsule.capsule(cap_radius: cr_b) = cap_b, position: p_b, orientation: o_b)
       ) do
-    :ok
+      {capsule_local_a_1, capsule_local_a_2} = Capsule.get_principle_points(cap_a)
+      {capsule_local_b_1, capsule_local_b_2} = Capsule.get_principle_points(cap_b)
+      capsule_world_a_1 = o_b |> Quatern.transform_vector(capsule_local_a_1) |> Vec3.add(p_a)
+      capsule_world_a_2 = o_b |> Quatern.transform_vector(capsule_local_a_2) |> Vec3.add(p_a)
+      capsule_world_b_1 = o_b |> Quatern.transform_vector(capsule_local_b_1) |> Vec3.add(p_b)
+      capsule_world_b_2 = o_b |> Quatern.transform_vector(capsule_local_b_2) |> Vec3.add(p_b)
+
+      {dist, cap_a_nearest, cap_b_nearest} = GUtil.nearest_points_for_segments({capsule_world_a_1, capsule_world_a_2}, {capsule_world_b_1, capsule_world_b_2})
+
+      cond do
+        dist > cr_a + cr_b -> :no_intersection
+        dist < @verysmol -> :coincident
+        true ->
+          # we need to see if the capsules are parallel or not,
+          a_spine = Vec3.subtract(capsule_world_a_2, capsule_world_a_1)
+          b_spine = Vec3.subtract(capsule_world_b_2, capsule_world_b_1)
+          a_spine_lengthsq = Vec3.length_squared(a_spine)
+          b_spine_lengthsq = Vec3.length_squared(b_spine)
+          if a_spine_lengthsq < @verysmol or b_spine_lengthsq < @verysmol do
+            # degenerate capsules :(, 1 contact point
+          else
+            # happy caqpsules :)
+            a_axis = Vec3.scale(a_spine, :math.sqrt(a_spine_lengthsq)
+            b_axis = Vec3.scale(b_spine, :math.sqrt(b_spine_lengthsq)
+            capsule_angle = :math.acos( Vec3.dot(a_axis, b_axis))
+            if :math.fmod( capsule_angle, :math.pi) > @verysmol do
+              # non-parallel capsules, 1 contact poiont
+            else
+              # parallel (stacked!) capsules, 2 contact points
+            end
+          end
+      end
   end
 end
