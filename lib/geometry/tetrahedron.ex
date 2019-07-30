2a478d9466598324b9d1f4ca74538e4a82bb0381
Chris Ertel <chris@kedagital.com>
2019-07-29 23:07:13 -0400

    Add Tetrahedron.to_barycentric/2.

--- lib/geometry/tetrahedron.ex
+++ lib/geometry/tetrahedron.ex
@@ -46,6 +46,69 @@ defmodule ElixirRigidPhysics.Geometry.Tetrahedron do
     (1.0 / 6.0) * Vec3.scalar_triple(v_bc, v_dc, v_ac)
   end
 
+  @doc """
+  Calculates the Barycentric coordinates in tetrahedron `t` for a query point `p`.
 
+  ## Examples
+    iex> # IO.puts "Check query point coincident with a."
+    iex> require ElixirRigidPhysics.Geometry.Tetrahedron, as: Tetra
+    iex> a = {1.0, 1.0, 1.0}
+    iex> b = {2.0, 1.0, 1.0}
+    iex> c = {1.0, 2.0, 1.0}
+    iex> d = {1.0, 1.0, 2.0}
+    iex> t = Tetra.create(a,b,c,d)
+    iex> Tetra.to_barycentric(t, a)
+    {1.0, 0.0, 0.0, 0.0}
+
+    iex> # IO.puts "Check query point coincident with b."
+    iex> require ElixirRigidPhysics.Geometry.Tetrahedron, as: Tetra
+    iex> a = {1.0, 1.0, 1.0}
+    iex> b = {2.0, 1.0, 1.0}
+    iex> c = {1.0, 2.0, 1.0}
+    iex> d = {1.0, 1.0, 2.0}
+    iex> t = Tetra.create(a,b,c,d)
+    iex> Tetra.to_barycentric(t, b)
+    {0.0, 1.0, 0.0, 0.0}
+
+    iex> # IO.puts "Check query point coincident with c."
+    iex> require ElixirRigidPhysics.Geometry.Tetrahedron, as: Tetra
+    iex> a = {1.0, 1.0, 1.0}
+    iex> b = {2.0, 1.0, 1.0}
+    iex> c = {1.0, 2.0, 1.0}
+    iex> d = {1.0, 1.0, 2.0}
+    iex> t = Tetra.create(a,b,c,d)
+    iex> Tetra.to_barycentric(t, c)
+    {0.0, 0.0, 1.0, 0.0}
+
+    iex> # IO.puts "Check query point coincident with d."
+    iex> require ElixirRigidPhysics.Geometry.Tetrahedron, as: Tetra
+    iex> a = {1.0, 1.0, 1.0}
+    iex> b = {2.0, 1.0, 1.0}
+    iex> c = {1.0, 2.0, 1.0}
+    iex> d = {1.0, 1.0, 2.0}
+    iex> t = Tetra.create(a,b,c,d)
+    iex> Tetra.to_barycentric(t, d)
+    {0.0, 0.0, 0.0, 1.0}
+
+    iex> # IO.puts "Check query point outside face BCD."
+    iex> require ElixirRigidPhysics.Geometry.Tetrahedron, as: Tetra
+    iex> a = {1.0, 1.0, 1.0}
+    iex> b = {2.0, 1.0, 1.0}
+    iex> c = {1.0, 2.0, 1.0}
+    iex> d = {1.0, 1.0, 2.0}
+    iex> t = Tetra.create(a,b,c,d)
+    iex> Tetra.to_barycentric(t, {2.0, 2.0, 2.0})
+    {-2.0, 1.0, 1.0, 1.0}
+  """
+  @spec to_barycentric(tetrahedron, Vec3.vec3) :: { float, float, float, float}
+  def to_barycentric( tetrahedron(a: a, b: b, c: c, d: d) = t, q ) do
+    v_qbcd = create(q,b,c,d) |> volume() # a
+    v_qacd = create(q,c,a,d) |> volume() # b
+    v_qabd = create(q,a,b,d) |> volume() # c
+    v_qcab = create(q,a,c,b) |> volume() # d
+    v_total = volume(t)
+
+    { v_qbcd / v_total, v_qacd / v_total, v_qabd / v_total, v_qcab / v_total}
+  end
 
 end
